91d5a4d1126ccc6306c4f83dd8a36c11ac750ae6
Azizbek <norboboyevazizbek0@gmail.com>
2024-06-13 23:40:10 +0500

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <div class="header">
-      <button @click="showModal = true">Modal</button>
     </div>
-    <div  class="table-container">
+    <div class="table-container">
       <table class="bordered">
         <thead>
           <tr>
@@ -12,21 +11,39 @@
         </thead>
         <tbody>
           <tr v-for="data in tableData" :key="data.id">
-            <td v-for="column in displayedColumns" :key="column.key">{{ data[column.key] }}</td>
+            <td v-for="column in displayedColumns" :key="`${data.id}-${column.key}`" :style="{ width: column.size+ 'px' }">{{ data[column.key] }}</td>
           </tr>
         </tbody>
       </table>
       <div class="checkbox-container">
         <div v-for="item in header" :key="item.name" class="checkbox-item">
           <input type="checkbox" v-model="item.check" />
+          <i class="fas fa-pencil-alt edit" @click="showModal = true"></i>
           <p>{{ item.name }}</p>
         </div>
       </div>
     </div>
     <div v-if="showModal" class="modal-container">
       <div class="modal-content">
-        <h2>Modal</h2>
-        <button @click="showModal = false">Close</button>
+        <div>
+          <table class="bordered" style="width: 100%;">
+            <thead>
+              <tr>
+                <th>Header Name</th>
+                <th>Size</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="column in header" :key="column.key">
+                <td><input v-model="column.name" /></td>
+                <td><input v-model="column.size" /></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div style="margin-top: 20px;">
+          <button @click="showModal = false">Close</button>
+        </div>
       </div>
     </div>
   </div>
@@ -38,17 +55,17 @@ export default {
   data() {
     return {
       showModal: false,
-      header:[
-        { name: 'Name', key: 'name', check: true},
-        { name: 'Age', key: 'age', check: true},
-        { name: 'Email', key: 'email', check: true},
-        { name: 'Country', key: 'country', check: true},
-        { name: 'Occupation', key: 'occupation', check: true},
+      header: [
+        { name: 'Name', key: 'name', check: true, size: '150' },
+        { name: 'Age', key: 'age', check: true, size: '50' },
+        { name: 'Email', key: 'email', check: true, size: '200' },
+        { name: 'Country', key: 'country', check: true, size: '100' },
+        { name: 'Occupation', key: 'occupation', check: true, size: '150' }
       ],
       tableData: [
-      { id: 1, name: 'John Doe', age: 30, email: 'john@example.com', country: 'USA', occupation: 'Engineer' },
-      { id: 2, name: 'Jane Smith', age: 25, email: 'jane@example.com', country: 'Canada', occupation: 'Doctor' },
-      { id: 3, name: 'Bob Johnson', age: 35, email: 'bob@example.com', country: 'UK', occupation: 'Teacher' }
+        { id: 1, name: 'John Doe', age: 30, email: 'john@example.com', country: 'USA', occupation: 'Engineer' },
+        { id: 2, name: 'Jane Smith', age: 25, email: 'jane@example.com', country: 'Canada', occupation: 'Doctor' },
+        { id: 3, name: 'Bob Johnson', age: 35, email: 'bob@example.com', country: 'UK', occupation: 'Teacher' }
       ]
     }
   },
@@ -72,7 +89,6 @@ export default {
 .table-container {
   display: flex;
   gap: 200px;
-
   margin-top: 20px;
   text-align: center;
 }
@@ -90,17 +106,18 @@ table.bordered th, table.bordered td {
 
 .checkbox-container {
   display: flex;
-  flex-direction: column;   
+  flex-direction: column;
 }
+
 .checkbox-item {
   display: flex;
   align-items: center;
+  gap: 10px;
 }
 
 .checkbox-item input {
   margin-right: 5px;
 }
-
 
 .modal-container {
   position: fixed;
@@ -114,7 +131,15 @@ table.bordered th, table.bordered td {
   align-items: center;
 }
 
+.edit {
+  cursor: pointer;
+  font-size: 13px;
+  color: rgb(255, 136, 0);
+}
+
 .modal-content {
+  width: 80%;
+  max-width: 600px;
   background-color: white;
   padding: 20px;
   border-radius: 5px;
