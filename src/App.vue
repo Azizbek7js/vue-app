939e36609033ed19fcf2f742301048b596ab5540
UlugbekNG <andynestrov@yandex.ru>
2024-06-20 18:46:37 +0500

    add pagenation

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,23 @@
       <button class="btn btn-danger" @click="changeStatus('INACTIVE')">INACTIVE</button>
     </div>
     <div class="table-container">
-      <table class="bordered">
-        <thead>
+      <div>
+        <table class="bordered">
+          <thead>
           <tr>
             <th v-for="column in displayedColumns" :key="column.name" :style="{ width: column.size < 1000 ? column.size + 'px': '1000px' }">{{ column.name }}</th>
           </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
           <tr v-for="data in filteredData" :key="data.id">
             <td v-for="column in displayedColumns" :key="`${data.id}-${column.key}`" >{{ data[column.key] }}</td>
           </tr>
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+        <div class=" d-flex align-items-center justify-content-end p-2">
+          <button class="btn border rounded mx-1" v-for="pageNumber in pages" @click="page = pageNumber" :class="{'btn-primary':pageNumber === page}"  :key="pageNumber"> {{pageNumber}} </button>
+        </div>
+      </div>
       <div class="checkbox-container">
         <div v-for="item in header" :key="item.name" class="checkbox-item">
           <input type="checkbox" v-model="item.check" />
@@ -641,6 +646,9 @@ export default {
       inputCheck: false,
       search: '',
       formattedInputValue : '',
+      page: 1,
+      perPage: 5,
+      pages: [],
       header: [
         { name: 'Name', key: 'name', check: true, size: 20 },
         { name: 'Age', key: 'age', check: true, size: 10 },
@@ -653,9 +661,18 @@ export default {
         { id: 1, name: 'John Doe', age: 30,status: 'ACTIVE', email: 'john@example.com', country: 'USA', occupation: 'Engineer' },
         { id: 2, name: 'Jane Smith', age: 25,status: 'ACTIVE', email: 'jane@example.com', country: 'Canada', occupation: 'Doctor' },
         { id: 3, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
-        { id: 3, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
-        { id: 2, name: 'Jane Smith', age: 25,status: 'ACTIVE', email: 'jane@example.com', country: 'Canada', occupation: 'Doctor' },
-        { id: 3, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 4, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 5, name: 'Jane Smith', age: 25,status: 'ACTIVE', email: 'jane@example.com', country: 'Canada', occupation: 'Doctor' },
+        { id: 6, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 7, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 8, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 9, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 10, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 11, name: 'Bob ', age: 22,status: 'INACTIVE' , email: 'jane@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 12, name: ' Johnson', age: 35,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 13, name: 'Bob Johnson', age: 10,status: 'INACTIVE' , email: 'bob@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 14, name: 'Jane Smith', age: 15,status: 'INACTIVE' , email: 'jane@example.com', country: 'UK', occupation: 'Teacher' },
+        { id: 15, name: 'Bob Johnson', age: 35,status: 'INACTIVE' , email: 'jane@example.com', country: 'UK', occupation: 'Teacher' },
       ]
     }
   },
@@ -665,6 +682,9 @@ export default {
         localStorage.setItem('header', JSON.stringify(this.header));
       },
       deep: true
+    },
+    posts () {
+      this.setPages();
     }
   },
   computed: {
@@ -674,7 +694,12 @@ export default {
     filteredData() {
       const status = this.status;
       const search = this.search.toLowerCase();
-      return this.tableData.slice().sort((a, b) => a.age - b.age).filter(item => {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = (page * perPage) - perPage;
+      let to = (page * perPage);
+      let data = this.tableData.slice(from, to);
+      return data.slice().sort((a, b) => a.age - b.age).filter(item => {
         if (status !== 'ALL' && item.status !== status) {
           return false;
         }
@@ -683,11 +708,16 @@ export default {
         );
       });
     }
-
   },
   methods: {
     closeModal() {
       this.modalOpen = false;
+    },
+    setPages () {
+      let numberOfPages = Math.ceil(this.tableData.length / this.perPage);
+      for (let index = 1; index <= numberOfPages; index++) {
+        this.pages.push(index);
+      }
     },
     changeStatus(status){
       this.status = status
@@ -714,7 +744,7 @@ export default {
     }
   },
   mounted() {
-    localStorage.setItem('header', JSON.stringify(this.header));
+    this.setPages()
     this.header = JSON.parse(localStorage.getItem('header')) || this.header;
     document.addEventListener('keydown', this.handleKeydown);
   },
@@ -733,7 +763,7 @@ export default {
   height: auto;
   display: flex;
   gap: 20px;
-  margin-top: 20px; 
+  margin-top: 20px;
   text-align: center;
 }
 .tabs-container{
